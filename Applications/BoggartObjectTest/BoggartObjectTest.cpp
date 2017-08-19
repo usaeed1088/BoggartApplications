40e46c83d230f616d1e7cad2a324299afac1cfbe
Usman Saeed <usman-syed@live.com>
2017-08-19 22:41:19 +1000

    Updated BoggartObjectTest.cpp

--- Applications/BoggartObjectTest/BoggartObjectTest.cpp
+++ Applications/BoggartObjectTest/BoggartObjectTest.cpp
@@ -12,22 +12,26 @@ std::shared_ptr<Boggart::Boggart> CreateServer(std::string id)
 	boggart->InjectTransport(std::shared_ptr<Boggart::Transport::TransportBase>(new Boggart::Transport::TCP::Server(IP, Port)));
 	boggart->InjectTimerManager(std::shared_ptr<Boggart::Timer::ManagerBase>(new Boggart::Timer::SoftTimerManager()));
 
-	boggart->Logger()->EnableModule(std::string("ConnectionManager"));
+	//boggart->Logger()->EnableModule(std::string("ConnectionManager"));
+	boggart->Logger()->EnableLevel(Boggart::Logger::Level::Information);
+	boggart->Logger()->EnableLevel(Boggart::Logger::Level::Warning);
 
 	return boggart;
 }
 
 std::shared_ptr<Boggart::Boggart> CreateClient(std::string id)
 {
-	std::shared_ptr<Boggart::Boggart> boggart(new Boggart::Boggart("Mr.Server"));
+	std::shared_ptr<Boggart::Boggart> boggart(new Boggart::Boggart("Mr.Client"));
 
 	boggart->InjectIPC(std::shared_ptr<Boggart::IPC::IPCBase>(new Boggart::IPC::Asynchronous(id)));
 	boggart->InjectLogger(std::shared_ptr<Boggart::Logger::LoggerBase>(new Boggart::Logger::Console()));
 	boggart->InjectTransport(std::shared_ptr<Boggart::Transport::TransportBase>(new Boggart::Transport::TCP::Client(id, IP, Port)));
 	boggart->InjectTimerManager(std::shared_ptr<Boggart::Timer::ManagerBase>(new Boggart::Timer::SoftTimerManager()));
 
-	boggart->Logger()->EnableModule(std::string("ConnectionManager"));
-	boggart->Logger()->EnableCategory(std::string("Transport"));
+	//boggart->Logger()->EnableModule(std::string("ConnectionManager"));
+	boggart->Logger()->EnableLevel(Boggart::Logger::Level::Information);
+	boggart->Logger()->EnableLevel(Boggart::Logger::Level::Warning);
+	//boggart->Logger()->EnableCategory(std::string("Transport"));
 
 	return boggart;
 }
@@ -39,10 +43,7 @@ int main(int argc, char* argv[])
 	auto client1 = CreateClient("90");
 	auto client2 = CreateClient("93");
 
-	server->InjectBoggart(client1);
-	server->InjectBoggart(client2);
-
-	server->Start();
+	Boggart::Boggart::Start();
 
 	return 0;
 }
